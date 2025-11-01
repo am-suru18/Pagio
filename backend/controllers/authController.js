c2e65fcb7e0f527d36adf4702b424301b0a24123
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-01 16:56:15 +0530

    defined the profile route and controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,6 +13,26 @@ exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    if (user) {
+      res.status(201).json({
+        message: "User successfully registered",
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
@@ -23,6 +43,20 @@ exports.registerUser = async (req, res) => {
 // @access - Public
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      message: "Login successfull",
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
 
   try {
   } catch (error) {
@@ -35,6 +69,15 @@ exports.loginUser = async (req, res) => {
 // @access - Private
 exports.getProfile = async (req, res) => {
   try {
+    const user = await User.findById(req.user.id);
+
+    res.json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      isPro: user.isPro
+    })
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
