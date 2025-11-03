dbc20ab809ccf495dc3707becc62b270ce25def5
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-03 23:18:49 +0530

    fix(exportRoutes): debug the importing of export controllers

--- backend/routes/exportRoutes.js
+++ backend/routes/exportRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router();
-const {exportAsPDFk exportAsDocument} = require("../controllers/exportController")
+const {exportAsPDFk, exportAsDocument} = require("../controllers/exportController")
 const {protect} = require("../middlewares/authMiddleware");
 const { route } = require("./aiRoutes");
 
