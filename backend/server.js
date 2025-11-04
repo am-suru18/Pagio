8d4a81ad31c939fcfd7c367bad8a643f4378de9a
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-04 20:48:45 +0530

    Feat(server): Connected routes of export controller

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,19 @@ const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const bookRoutes = require("./routes/bookRoutes");
-const aiRoutes = require("./routes/aiRoutes")
+const aiRoutes = require("./routes/aiRoutes");
+const exportRoutes = require("./routes/exportRoutes");
 
 const app = express();
 
 //Middleware to handle CORS
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}))
-
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 //Connect to Database
 connectDB();
@@ -31,10 +33,11 @@ app.use("/backend/uploads", express.static(path.join(__dirname, "uploads")));
 //Routes Here
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
-app.use("/api/ai", aiRoutes)
+app.use("/api/ai", aiRoutes);
+app.use("/api/export", exportRoutes);
 
 //Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
