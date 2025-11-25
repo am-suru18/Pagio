51228b2380588ca898714f8fc4474232324bb8dc
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-25 19:45:31 +0530

    feat(DashboardPage): implement necessary imports

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import {useNavigate} from "react-router-dom"
+import toast from "react-hot-toast"
+import {Plus, Book } from "lucide-react"
+
+import DashboardLayout from "../components/layout/DashboardLayout.jsx"
+import Button from "../components/ui/Button.jsx"
+import {useAuth} from "../context/AuthContext.jsx"
+import axiosInstance from "../utils/apiPaths.js"
 
 const DashboardPage = () => {
     return <div>DashboardPage</div>;
