4147f9ff484cbae2712adf6bde738aec96afd582
Suraj Chaudhary <am.suru18@gmail.com>
2025-11-29 16:02:24 +0530

    feat(Dropdown): add useEffect hook

---
+++ frontend/src/components/ui/Dropdown.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const Dropdown = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target)
+            ) {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+    return <div></div>;
+};
+
+export default Dropdown;
