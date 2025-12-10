633c482ed5a02c4bd11533ffe856835e82c94ab8
Suraj Chaudhary <am.suru18@gmail.com>
2025-12-10 17:45:28 +0530

    chore(ChapterEditor): add necessary imports

--- frontend/src/components/editor/ChapterEditorTab.jsx
+++ frontend/src/components/editor/ChapterEditorTab.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import { useMemo, useState } from 'react';
+import { Sparkles, Type, Eye, Maximize2 } from 'lucide-react';
+import Button from '../ui/Button';
+import InputField from '../ui/InputField';
+import SimpleMDEditor from './SimpleMDEditor';
 
 const ChapterEditorTab = ({
-    book,
-    selectedChapterIndex,
-    handleChapterChange,
-    onGenerateChapterContent,
+    book = {
+        title: 'Untitled',
+        chapter: [
+            {
+                title: 'Chapter 1',
+                content: '-'
+            }
+        ]
+    },
+    selectedChapterIndex = 0,
+    handleChapterChange = () => {},
+    onGenerateChapterContent = () => {},
     isGenerating
 }) => {
     return <div className=""></div>;
